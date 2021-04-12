d45ab32e28ddeabc0d55f48ed03d11e51be3a3d1
xzzxwmsl <1365303748@qq.com>
2021-04-12 22:51:22 +0800

    第一次作业

--- 1/README.md
+++ 1/README.md
@@ -2,16 +2,33 @@
 |-|-|
 |向正中|2020223040024|
 
-# 第一次作业
+# 第一次作业 PMF
+PMF项目复现了论文《Probabilistic Matrix Factorization》
+
 代码参考开源代码，修改为了momentum梯度下降法
+
+结果：对U、V进行了更新并计算了loss。最后，对指定的用户（user_id）进行了top 5的电影推荐。 
+
+## 论文信息
+U,V更新
+
+![](UV更新.png)
+
+目标函数
+
+![](目标函数.png)
+
 ## 参数
-|Lambda|feat(D的维度)|epsilon|alpha|moment|epoch|batch|batch_size|
-|-|-|-|-|-|-|-|-|
-|0.1|10|1|0.1|0.8|100|1000|100|
+|Lambda|D|learning_rate|epoch|
+|-|-|-|-|
+|0.1|10|0.005|50|
 
-
+训练集 : 测试集=8:2
 
 ## 运行截图
-![](结果_1.png)
-
-![](结果_2.png)
+### RSME曲线
+![](运行截图1_RSME.png)
+### 具体参数
+![](运行截图2_rsme.png)
+### 推荐结果
+![](推荐结果.png)
--- 1/pmf.py
+++ 1/pmf.py
@@ -1,155 +1,153 @@
-from numpy import *
-import random
 import numpy as np
+import random
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.model_selection import train_test_split
+from pylab import *
+import matplotlib
 import matplotlib.pyplot as plt
 
 
 class PMF(object):
-    def __init__(self, Lambda=0.1, feat=10, epsilon=1, alpha=0.1, momentum=0.8, epoch=100, batch=1000, batch_size=100):
-        self.Lambda = Lambda  # 正则
-        self.feat = feat  # 潜在特征数,即 D
-        self.epsilon = epsilon
-        self.alpha = alpha  # 学习率
-        self.epoch = epoch  # epoch
-        self.batch = batch  # 每个epoch更新次数
-        self.batch_size = batch_size  # 样本数
-        self.momentum = momentum  # 增量
-        self.U = None  # 用户潜在特征向量
-        self.V = None  # 电影潜在特征向量
+    def __init__(self, Lambda=0.1, D=10, learning_rate=0.005, epoch=5):
+        self.Lambda = Lambda
+        self.D = D  # 潜在特征维度
+        self.M = None  # 电影数
+        self.N = None  # 用户数
+        self.learning_rate = learning_rate
+        self.epoch = epoch
+        self.train_list = None
+        self.test_list = None
+        self.data = None
+        self.rated_movie_id_dict = None
 
-        # 均方根误差
-        self.rmse_train = []
-        self.rmse_test = []
-
-    # 读取数据
     def read_data(self, path='C:\\Users\\13653\\Desktop\\RCS\\1\\u.data'):
         data = []
+        N, M, u_idx, m_idx = 0, 0, 0, 0
+        user_id_dict, rated_movie_id_dict = {}, {}
+        index2movie = {}
+        index2user = {}
         for i in open(path, 'r'):
+            # 用户id，电影id，评分
             (uid, mid, rat, _) = i.split('\t')
-            uid = int(uid)
-            mid = int(mid)
-            rat = float(rat)
-            data.append([uid, mid, rat])
-        return array(data)
+            if int(uid) not in user_id_dict:
+                user_id_dict[int(uid)] = u_idx
+                index2user[u_idx] = int(uid)
+                u_idx += 1
+            if int(mid) not in rated_movie_id_dict:
+                rated_movie_id_dict[int(mid)] = m_idx
+                index2movie[m_idx] = int(mid)
+                m_idx += 1
+                # [用户id位置，电影id位置，评分]
+            data.append([user_id_dict[int(uid)],
+                         rated_movie_id_dict[int(mid)], float(rat)])
+        # 用户数和电影数
+        N = u_idx
+        M = m_idx
+        self.user_id_dict = user_id_dict
+        print("用户数%f, 电影数%f,数据集长度%f" % (N, M, len(data)))
+        return N, M, data, index2movie, index2user
 
-    # 预处理数据
-    def pre_handle_data(self, data, test_size=0.2):
-        train_data = []
-        test_data = []
-        for i in data:
-            rand = random.random()
-            if rand > test_size:
-                train_data.append(i)
-            else:
-                test_data.append(i)
-        return array(train_data), array(test_data)
+    def list2mat(self, sequence, N, M):
+        records_array = np.array(sequence)
+        mat = np.zeros([N, M])
+        row = records_array[:, 0].astype(int)
+        col = records_array[:, 1].astype(int)
+        values = records_array[:, 2].astype(np.float32)
+        mat[row, col] = values
+        return mat
 
-    def train(self, train_v, test_v):
-        self.mean_inv = np.mean(train_v[:, 2])  # 评分的均值
-        train_size, test_size = train_v.shape[0], test_v.shape[0]  # 训练集和测试集的大小
+    def initParameters(self):
+        self.N, self.M, self.data, self.index2movie,self.index2user = self.read_data()
+        self.train_list, self.test_list = train_test_split(
+            self.data, test_size=0.2)
 
-        # 求出用户和电影的总数
-        num_user = int(max(np.amax(train_v[:, 0]), np.amax(test_v[:, 0]))) + 1
-        num_movie = int(max(np.amax(train_v[:, 1]), np.amax(test_v[:, 1]))) + 1
-        # 因为其向量分别在第0列和第1列(其id值按增序0~n)
+    def train(self):
+        U = np.random.normal(0, 0.1, (self.N, self.D))
+        V = np.random.normal(0, 0.1, (self.M, self.D))
 
-        self.V = 0.1 * np.random.randn(num_movie, self.feat)  # 电影 M x D 正态分布矩阵
-        self.U = 0.1 * np.random.randn(num_user, self.feat)  # 电影 M x D 正态分布矩阵
-        # 创建电影和用户的 M x D 0矩阵，用于增量梯度下降
-        self.V_inc = np.zeros((num_movie, self.feat))
-        self.U_inc = np.zeros((num_user, self.feat))
+        train_mat = self.list2mat(sequence=self.train_list, N=self.N, M=self.M)
+        test_mat = self.list2mat(sequence=self.test_list, N=self.N, M=self.M)
 
-        for epoch_now in range(0, self.epoch):
-            # 打乱训练集元组
-            shuffled_order = np.arange(train_v.shape[0])  # 生成等差数列0~n
-            np.random.shuffle(shuffled_order)  # 打乱顺序
+        records = []
 
-            # 梯度更新
-            for batch in range(self.batch):
-                # 获取此次需要学习的元素下标
-                test = np.arange(self.batch_size * batch,
-                                 self.batch_size * (batch + 1))
-                batch_index = np.mod(
-                    test, shuffled_order.shape[0])  # 本次迭代要使用的索引下标
-                batch_UserID = np.array(
-                    train_v[shuffled_order[batch_index], 0], dtype='int32')
-                batch_MovieID = np.array(
-                    train_v[shuffled_order[batch_index], 1], dtype='int32')
+        for step in range(self.epoch):
+            los = 0
+            for data in self.train_list:
+                user, movie, rate = data
+                U[user], V[movie], ls = self.update(U[user], V[movie], rate=rate,
+                                                    learning_rate=self.learning_rate,
+                                                    Lambda=self.Lambda)
+                los += ls
+            pred_mat = self.prediction(U, V)
+            rsme_train, rsme_test = self.evaluation(
+                pred_mat, train_mat, test_mat)
+            records.append(np.array([los, rsme_train, rsme_test]))
 
-                # 计算目标函数
-                pred_out = np.sum(np.multiply(
-                    self.U[batch_UserID, :], self.V[batch_MovieID, :]), axis=1)
-                # 加平均值
-                rawErr = pred_out - \
-                    train_v[shuffled_order[batch_index], 2] + self.mean_inv
+            print(' step:%d \n loss:%.4f,rmse_train:%.4f,rmse_tese:%.4f'
+                  % (step, los, rsme_train, rsme_test))
 
-                # 计算梯度
-                Ix_U = 2 * np.multiply(rawErr[:, np.newaxis], self.V[batch_MovieID, :]) \
-                    + self.Lambda * self.U[batch_UserID, :]
-                Ix_V = 2 * np.multiply(rawErr[:, np.newaxis], self.U[batch_UserID, :]) \
-                    + self.Lambda * (self.V[batch_MovieID, :])
-                dw_V = np.zeros((num_movie, self.feat))
-                dw_U = np.zeros((num_user, self.feat))
+        print(' end. \n loss:%.4f,rsme_train:%.4f,rmse_test:%.4f'
+              % (records[-1][0], records[-1][1], records[-1][2]))
+        return U, V, np.array(records)
 
-                # 计算对于同一个用户或者商品的梯度和，然后统一进行更新
-                for i in range(self.batch_size):
-                    dw_V[batch_MovieID[i], :] += Ix_V[i, :]
-                    dw_U[batch_UserID[i], :] += Ix_U[i, :]
+    def update(self, u, v, rate, learning_rate, Lambda):
+        error = rate - np.dot(u, v.T)
+        u = u + learning_rate * (error * v - Lambda * u)
+        v = v + learning_rate * (error * u-Lambda * v)
+        loss = 0.5 * (error**2 + Lambda *
+                      (np.square(u).sum() + np.square(v).sum()))
+        return u, v, loss
 
-                # 增量更新
-                self.V_inc = self.momentum * self.V_inc + self.epsilon * dw_V / self.batch_size
-                self.U_inc = self.momentum * self.U_inc + self.epsilon * dw_U / self.batch_size
+    def prediction(self, U, V):
+        N, D = U.shape
+        M, D = V.shape
+        rate_list = []
 
-                self.V = self.V - self.V_inc * self.alpha
-                self.U = self.U - self.U_inc * self.alpha
+        for u in range(N):
+            u_rate = np.sum(U[u, :]*V, axis=1)
+            rate_list.append(u_rate)
+        pred_result = np.array(rate_list)
+        return pred_result
 
-                # 计算训练集误差
-                if batch == self.batch - 1:
-                    pred_out = np.sum(np.multiply(self.U[np.array(train_v[:, 0], dtype='int32'), :],
-                                                  self.V[np.array(train_v[:, 1], dtype='int32'), :]),
-                                      axis=1)  # mean_inv subtracted
-                    rawErr = pred_out - train_v[:, 2] + self.mean_inv
-                    obj = np.linalg.norm(rawErr) ** 2 \
-                        + 0.5 * \
-                        self.Lambda * (np.linalg.norm(self.U)
-                                        ** 2 + np.linalg.norm(self.V) ** 2)
+    def evaluation(self, pred_mat, train_mat, test_mat):
+        y_pred_test = pred_mat[test_mat > 0]
+        y_true_test = test_mat[test_mat > 0]
+        rmse_test = np.sqrt(mean_squared_error(y_true_test, y_pred_test))
 
-                    self.rmse_train.append(np.sqrt(obj / train_size))
+        y_pred_train = pred_mat[train_mat > 0]
+        y_true_train = train_mat[train_mat > 0]
+        rmse_train = np.sqrt(mean_squared_error(y_pred_train, y_true_train))
 
-                # Compute validation error
-                if batch == self.batch - 1:
-                    pred_out = np.sum(np.multiply(self.U[np.array(test_v[:, 0], dtype='int32'), :],
-                                                  self.V[np.array(test_v[:, 1], dtype='int32'), :]),
-                                      axis=1)  # mean_inv subtracted
-                    rawErr = pred_out - test_v[:, 2] + self.mean_inv
-                    self.rmse_test.append(np.linalg.norm(
-                        rawErr) / np.sqrt(test_size))
-
-                    # Print info
-                    if batch == self.batch - 1:
-                        print('Training RMSE: %f, Test RMSE %f' %
-                              (self.rmse_train[-1], self.rmse_test[-1]))
+        return rmse_train, rmse_test
 
 
 if __name__ == "__main__":
+    print('begin')
     file_path = "C:\\Users\\13653\\Desktop\\RCS\\1\\u.data"
-    pmf = PMF()
-    ratings = pmf.read_data(file_path)
-    print(len(np.unique(ratings[:, 0])), len(
-        np.unique(ratings[:, 1])), pmf.feat)
-    train, test = train_test_split(
-        ratings, test_size=0.2)  # spilt_rating_dat(ratings)
-    pmf.train(train, test)
+    pmf = PMF(Lambda=0.1, D=10, learning_rate=0.005, epoch=50)
+    pmf.initParameters()
+    U, V, records = pmf.train()
+    predict = pmf.prediction(U=U, V=V)
+    index2user = pmf.index2user
+    index2movie = pmf.index2movie
+    user_id_dict = pmf.user_id_dict
 
-    # Check performance by plotting train and test errors
-    plt.plot(range(pmf.epoch), pmf.rmse_train,
-             marker='o', label='Training Data')
-    plt.plot(range(pmf.epoch), pmf.rmse_test, marker='v', label='Test Data')
-    plt.title('The MovieLens Dataset Learning Curve')
-    plt.xlabel('Number of Epochs')
+    plt.plot(range(pmf.epoch), records[:,1], marker='.', label='Training Data')
+    plt.plot(range(pmf.epoch), records[:,2], marker='o', label='Test Data')
+    plt.title('MovieLens')
+    plt.xlabel('epoch')
     plt.ylabel('RMSE')
     plt.legend()
     plt.grid()
     plt.show()
-    # print("precision_acc,recall_acc:" + str(pmf.topK(test)))
+
+    userid = 1
+    while userid != -1:
+        userid = int(input('请输入要预测的用户id'))
+        userid_index = user_id_dict[userid]
+        print(userid_index)
+        # 预测评分前五
+        predict_seq = predict[userid_index-1].argsort()[-5:][::-1]
+        print('用户%d的推荐:'%(userid))
+        for i in predict_seq:
+            print('电影%d,评分%.2f'%(index2movie[i],predict[userid_index][i]))
